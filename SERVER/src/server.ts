ce7f68a380717b58b220329cd4277217bb705003
OliveiraLeonidas <leonidasoliv25@gmail.com>
2024-07-11 00:31:13 -0300

    feat: add activities and links routes

--- SERVER/src/server.ts
+++ SERVER/src/server.ts
@@ -4,6 +4,10 @@ import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod
 import { confirmTrip } from './routes/confirm-trip';
 import { createTrip } from './routes/create-trips';
 import { confirmParticipant } from './routes/confirm-participant';
+import { createActivity } from './routes/create-activity';
+import { getActivities } from './routes/get-activities';
+import { createLink } from './routes/create-link';
+import { getLinks } from './routes/get-links';
 const app = fastify()
 
 
@@ -20,6 +24,10 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipant)
+app.register(createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 const port = 3333
 
