d63368b0419c3de36560e3e4485a50f5f70d5a07
OliveiraLeonidas <leonidasoliv25@gmail.com>
2024-07-15 22:01:35 -0300

    feat: add link modal

---
+++ src/pages/trip-details/create-link-modal.tsx
@@ -0,0 +1,17 @@
+export function CreateLinkModal() {
+  return (
+    <div>
+      {/* <DayPicker
+          mode="range"
+          locale={ptBR}
+          selected={eventStartAndEndDates}
+          onSelect={setEventStartAndEndDates}
+          disabled={{
+            before: new Date(),
+          }}
+        /> */}
+    </div>
+  );
+}
+
+//http://localhost:3333/trips/ecfb1ecd-2385-4f0e-8ab2-7d449f013bb7/confirm
